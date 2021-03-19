d34118635d8a37c90e79eb141d505add72fcf3e9
hyeonji <hyeonjiiseon@gmail.com>
2021-03-19 14:06:30 +0900

    초기 속도 simParams.cV로 고정

--- LTEV2Vsim_v5.0_tbtg/LTEV2Vsim_v5.0/Simulations.m
+++ LTEV2Vsim_v5.0_tbtg/LTEV2Vsim_v5.0/Simulations.m
@@ -6,9 +6,10 @@ clc          % Clear the command window
 
 %% LTE Autonomous (3GPP Mode 4) - on a subframe basis
 % Autonomous allocation algorithm defined in 3GPP standard
-density = [200, 400, 600, 800];
-for i = 1:length(density)
-    LTEV2Vsim('BenchmarkPoisson.cfg','simulationTime',40,'roadLength', 3000, 'rho', density(i),...
-        'BRAlgorithm',18,'probResKeep',0);
-end
+%density = [50, 100, 200]; %density(i)
+
+%for i = 1:length(density)
+    LTEV2Vsim('BenchmarkPoisson.cfg','simulationTime',10, 'rho', 50,...
+        'BRAlgorithm',18);
+%end
 %'MCS_LTE', 7, 'printCBR', true
